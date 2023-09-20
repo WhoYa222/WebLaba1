50cf59d9a07d24d41168b01ec8f8043dbd220e40
WhoYa222 <144787939+WhoYa222@users.noreply.github.com>
2023-09-20 20:10:17 +0300

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -150,7 +150,7 @@ $(document).ready(function() {
 			<input readonly class="product5"></input>
 			<input readonly class="amount5"></input>
 			</div>`)
-			$('.product5').val("Diablo IVV");
+			$('.product5').val("Diablo IV");
 			$('.amount5').val(cart[i]);
 					break;
 				default:
